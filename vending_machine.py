46e7cdbc7595c00096edbe18bb326c5a31c32a26
Stephen Byrne <s.byrne6@gmail.com>
2019-06-19 11:10:52 +0000

    challenge solution added

--- vending_machine.py
+++ vending_machine.py
@@ -1,16 +1,31 @@
 from byotest import *
 
-usd_coins = [100, 50, 25, 10, 5, 1]
+extra_eur_coins = {100: 50, 50: 50, 25: 50, 10: 50, 5: 50, 1: 50}
 
-eur_coins = [100, 50, 20, 10, 5, 2, 1]
+eur_coins = {100: 20, 50: 20, 25: 20, 10: 20, 5: 20, 1: 20}
+
+usd_coins = {100: 20, 50: 20, 25: 20, 10: 20, 5: 20, 1: 20}
+
+# usd_coins = [100, 50, 25, 10, 5, 1]
+
+# eur_coins = [100, 50, 20, 10, 5, 2, 1]
 
 def get_change(amount, coins=eur_coins):
 
     change = []
-    for coin in coins:
-        while coin <= amount: 
-            amount -= coin
-            change.append(coin)
+    # for coin in coins:
+    #     while coin <= amount: 
+    #         amount -= coin
+    #         change.append(coin)
+    
+    for denomination in sorted(coins.keys(), reverse=True):
+        while denomination <= amount and coins[denomination] > 0:
+            amount -= denomination
+            coins[denomination] -= 1
+            change.append(denomination)
+            
+    if amount != 0:
+        raise Exception("Insufficient coins to give change.")
             
     return change
 
