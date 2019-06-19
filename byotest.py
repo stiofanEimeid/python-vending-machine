31bb32f6577b7275a3d249e00401b5a7a1a48cda
Stephen Byrne <s.byrne6@gmail.com>
2019-06-19 10:26:48 +0000

    initial commit

---
+++ byotest.py
@@ -0,0 +1,11 @@
+def test_not_equal(a, b):
+    assert a != b, "Did not expect {0} but got {1}".format(a,b)
+    
+def test_is_in(collection, item):
+    assert item in collection, "{0} does not contain {1}".format(collection, item)
+    
+def test_not_in(collection, item):
+    assert item not in collection, "{0} is in {1}".format(item, collection)
+    
+def test_between(upper_limit, lower_limit, actual):
+    assert lower_limit <= actual <= upper_limit, "{0} is not between {1} and {2}".format(actual, lower_limit, upper_limit)
---
+++ vending_machine.py
@@ -0,0 +1,3 @@
+from byotest import *
+
+print("All tests pass!")
